3e4735d63488d908292b9d67675227bd3624c693
VanHoi-28 <huynhvanhoi2811@gmail.com>
2024-04-03 09:03:51 +0700

    add

--- src/routers/web.js
+++ src/routers/web.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import homeController from '../controller/homeController';
 
 const router = express.Router();
 
@@ -8,9 +9,10 @@ const router = express.Router();
  * @param {*} app express app 
  */
 const initWebRouter = (app) => {
-    router.get("/", (req, res) => {
-        return res.send("Hello");
-    })
+
+    //path, handle
+    router.get("/", homeController.handleHelloWorld)
+    router.get("/user", homeController.handleUserPage)
 
     return app.use("/", router);
 }
